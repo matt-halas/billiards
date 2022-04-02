a18b6792956c4d40d817d952b2b09934a45d10b3
Mattyboiii <m.halas04@gmail.com>
2022-04-01 20:02:18 -0600

    Fixed pocket locations

--- billiards.py
+++ billiards.py
@@ -1,4 +1,5 @@
 
+from tkinter import CENTER
 import pygame
 import sys
 from settings import Settings
@@ -16,12 +17,12 @@ class BilliardsGame:
             self.screen_height))
         self.screen_rect = self.screen.get_rect()
 
-        self.border_thickness = 100
+        self.border_thickness = 50
 
-        self.table_width = self.screen_width - self.border_thickness
-        self.table_height = self.screen_height - self.border_thickness
-        self.table_rect = pygame.Rect(self.border_thickness / 2,
-            self.border_thickness / 2, self.table_width, self.table_height)
+        self.table_width = self.screen_width - 2 *self.border_thickness
+        self.table_height = self.screen_height - 2 * self.border_thickness
+        self.table_rect = pygame.Rect(self.border_thickness,
+            self.border_thickness, self.table_width, self.table_height)
         
 
 
@@ -33,21 +34,30 @@ class BilliardsGame:
         while True:
             for event in pygame.event.get():
                 if event.type == pygame.KEYDOWN:
-                    sys.exit()
+                    if event.key == pygame.K_q:
+                        sys.exit()
                 if event.type == pygame.QUIT:
                     sys.exit()
             self.screen.fill((51, 26, 0))
-            self.draw_pockets()
+            self.draw_side_pockets()
             self.draw_table()
+            self.draw_pockets()
             pygame.display.flip()
-    
+                
     def draw_pockets(self):
-        pocket_locations = [self.screen_rect.topleft, self.screen_rect.midtop,
-            self.screen_rect.topright, self.screen_rect.bottomright,
-            self.screen_rect.midbottom, self.screen_rect.bottomleft]
+        t = self.border_thickness
+        pocket_locations = [(t, t), (self.screen_rect.right - t, t),
+            (self.screen_rect.right - t, self.screen_rect.bottom - t),
+            (t, self.screen_rect.bottom - t)]
         for loc in pocket_locations:
             pygame.draw.circle(self.screen, (0, 0, 0), loc, 40)
+            continue
     
+    def draw_side_pockets(self):
+        t = self.border_thickness
+        pygame.draw.circle(self.screen, (0, 0, 0), (self.screen_rect.centerx, t), 40)
+        pygame.draw.circle(self.screen, (0, 0, 0), (self.screen_rect.centerx, self.screen_rect.bottom - t), 40)
+
     def draw_table(self):
         pygame.draw.rect(self.screen, (27, 77, 0), self.table_rect)
 
