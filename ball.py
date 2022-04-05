8089e8e9f248a42a2d009335e12f425f2eb49c30
Mattyboiii <m.halas04@gmail.com>
2022-04-04 20:09:33 -0600

    Added collisions

--- ball.py
+++ ball.py
@@ -1,21 +1,21 @@
 import pygame
 import numpy as np
-class CueBall:
-    '''Class for cue ball and cue ball related things'''
-    def __init__(self, billiards):
+
+class Ball:
+    def __init__(self, billiards, loc, color):
         self.settings = billiards.settings
         self.screen = billiards.screen
         self.screen_rect = self.screen.get_rect()
-        self.x = self.settings.screen_width / 3
-        self.y = self.settings.screen_height / 2
+        self.x, self.y = loc
         self.x_vel = 0
         self.y_vel = 0
         self.radius = 10
+        self.color = color
+
+    def draw_ball(self):
+        pygame.draw.circle(self.screen, (self.color), (self.x, self.y), self.radius)
     
-    def draw_cue_ball(self):
-        pygame.draw.circle(self.screen, (255, 255, 255), (self.x, self.y), self.radius)
-    
-    def update_cue_ball(self):
+    def update_ball(self):
         self.x += self.x_vel
         self.y += self.y_vel
         self.x_vel *= 0.9998
@@ -36,30 +36,30 @@ class CueBall:
         if (self.y - self.radius <= self.settings.border_thickness) and \
             ((self.x > self.screen_rect.centerx + self.settings.pocket_radius) or (self.x < self.screen_rect.centerx - self.settings.pocket_radius)):
             self.y_vel *= -1
-    
+
     def detect_pocket_collision(self):
         self.x_tab = self.x - self.settings.border_thickness
         self.y_tab = self.y - self.settings.border_thickness
         #Check for top left pocket
         if np.sqrt(self.x_tab**2 + self.y_tab**2) < self.settings.pocket_radius:
-            self.reset_cue_ball()
+            self.sink_ball()
         #Check for top right pocket
         if np.sqrt((self.settings.screen_width - 2 * self.settings.border_thickness
             - self.x_tab)**2 + self.y_tab**2) < self.settings.pocket_radius:
-            self.reset_cue_ball()
+            self.sink_ball()
         #Check for bottom right pocket
         if np.sqrt((self.settings.screen_width - 2 * self.settings.border_thickness - self.x_tab)**2
             + (self.settings.screen_height - 2 * self.settings.border_thickness - self.y_tab)**2) < self.settings.pocket_radius:
-            self.reset_cue_ball()
+            self.sink_ball()
         #Check for bottom left pocket
         if np.sqrt(self.x_tab**2 + (self.settings.screen_height - 2 * self.settings.border_thickness - self.y_tab)**2) < self.settings.pocket_radius:
-            self.reset_cue_ball()
+            self.sink_ball()
         #Check for side pockets
         if (self.y < self.settings.border_thickness) or (self.y > self.screen_rect.bottom - self.settings.border_thickness):
-            self.reset_cue_ball()
-
-    def reset_cue_ball(self):
-        self.x = self.settings.screen_width / 3
-        self.y = self.settings.screen_height / 2
+            self.sink_ball()
+        
+    def sink_ball(self):
+        self.x = -100
+        self.y = -100
         self.x_vel = 0
         self.y_vel = 0
--- billiards.py
+++ billiards.py
@@ -1,10 +1,10 @@
-
-from tkinter import CENTER
 import pygame
 import sys
+import numpy as np
 from settings import Settings
 from cue_ball import CueBall
 from cue import Cue
+from ball import Ball
 
 
 class BilliardsGame:
@@ -28,6 +28,7 @@ class BilliardsGame:
         
         self.cue_ball = CueBall(self)
         self.cue = Cue(self)
+        self.ball = Ball(self, (500, 200), (200, 0, 0))
 
         pygame.display.set_caption("Chilliard Billiard")
 
@@ -39,7 +40,11 @@ class BilliardsGame:
             self.draw_game()
             self.cue_ball.detect_wall_collision()
             self.cue_ball.detect_pocket_collision()
+            self.ball.detect_wall_collision()
+            self.ball.detect_pocket_collision()
+            self.detect_collisions()
             self.cue_ball.update_cue_ball()
+            self.ball.update_ball() 
             pygame.display.flip()
     
     def check_events(self):
@@ -62,6 +67,7 @@ class BilliardsGame:
         self.draw_table()
         self.draw_pockets()
         self.cue_ball.draw_cue_ball()
+        self.ball.draw_ball()
         self.cue.draw_cue(self.cue_ball)
 
     def draw_pockets(self):
@@ -80,6 +86,31 @@ class BilliardsGame:
 
     def draw_table(self):
         pygame.draw.rect(self.screen, (27, 77, 0), self.table_rect)
+    
+    def detect_collisions(self):
+        if np.sqrt((self.cue_ball.x - self.ball.x)**2 + (self.cue_ball.y - self.ball.y)**2) <= 2 * self.ball.radius:
+            self.collision(self.cue_ball, self.ball)
+    
+    def collision(self, ball_1, ball_2):
+        if ball_1.x_vel !=  0:
+            theta_1 = np.arctan2(ball_1.y_vel, ball_1.x_vel)
+        else:
+            theta_1 = np.pi / 2
+        
+        if ball_2.x_vel !=  0:
+            theta_2 = np.arctan2(ball_2.y_vel, ball_2.x_vel)
+        else:
+            theta_2 = np.pi / 2
+        
+        phi = np.arctan2(ball_2.y - ball_1.y, ball_2.x - ball_1.x)
+        v_1 = np.sqrt(ball_1.x_vel**2 + ball_1.y_vel**2)
+        v_2 = np.sqrt(ball_2.x_vel**2 + ball_2.y_vel**2)
+
+        ball_1.x_vel = v_2 * np.cos(theta_2 - phi) * np.cos(phi) + v_1 * np.sin(theta_1 - phi) * np.cos(phi + np.pi / 2)
+        ball_1.y_vel = v_2 * np.cos(theta_2 - phi) * np.sin(phi) + v_1 * np.sin(theta_1 - phi) * np.sin(phi + np.pi / 2)
+        ball_2.x_vel = v_1 * np.cos(theta_1 - phi) * np.cos(phi) + v_2 * np.sin(theta_2 - phi) * np.cos(phi + np.pi / 2)
+        ball_2.y_vel = v_1 * np.cos(theta_1 - phi) * np.sin(phi) + v_2 * np.sin(theta_2 - phi) * np.sin(phi + np.pi / 2)
+
 
 
 if __name__ == "__main__":
